c8a543ac4d5dd2246c8275296bfbdc064f4e31a1
jrperiod <jrperiod@vitalgroundz.com>
2016-06-21 19:57:36 -0400

    reset button

--- app/src/main/java/com/codetank/beginnerprogrammers/Forgot/ForgotFragment.java
+++ app/src/main/java/com/codetank/beginnerprogrammers/Forgot/ForgotFragment.java
@@ -9,9 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import com.codetank.beginnerprogrammers.LoginActivity;
 
 import com.codetank.beginnerprogrammers.R;
 import com.firebase.client.Firebase;
@@ -20,31 +17,73 @@ import com.firebase.client.FirebaseError;
 /**
  * Created by jrperiod on 6/15/16.
  */
-public class ForgotFragment extends Fragment {
+public class ForgotFragment extends Fragment implements View.OnClickListener{
 
     public static final String FIRE_BASE_URL = "https://bpgo.firebaseio.com/";
     public static final String USERS_ROUTE = "users/";
 
     Firebase mFireBaseRef;
 
-    private EditText userEmail;
+    private EditText userEmailReset;
     private Button resetBtn;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mFireBaseRef = new Firebase(FIRE_BASE_URL + USERS_ROUTE);
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.forgot_fragment, container, false);
 
-        userEmail = (EditText) layout.findViewById(R.id.userEmailET);
+        userEmailReset = (EditText) layout.findViewById(R.id.emailET);
         resetBtn = (Button) layout.findViewById(R.id.resetBtn);
 
-        //resetBtn.setOnClickListener(this);
+        resetBtn.setOnClickListener(this);
 
         return layout;
     }
 
+    public void resetPassword(){
+
+        if(!resetCredentialsValidated()){
+            return;
+        }
+
+        mFireBaseRef.resetPassword(userEmailReset.getText().toString(), new Firebase.ResultHandler() {
+            @Override
+            public void onSuccess() {
+                Log.d("flow", "Send to " + userEmailReset.getText().toString());
+            }
+
+            @Override
+            public void onError(FirebaseError firebaseError) {
+                Log.d("flow", "ERROR: " + firebaseError.toString() + " " + firebaseError.getMessage() + " " + firebaseError.getDetails());
+            }
+        });
 
     }
 
+    public boolean resetCredentialsValidated(){
+        boolean isValid = true;
 
+        if(!android.util.Patterns.EMAIL_ADDRESS.matcher(userEmailReset.getText().toString()).matches()){
+            isValid = false;
+        }
+
+        return  isValid;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onClick(View v) {
+            resetPassword();
+    }
+}
